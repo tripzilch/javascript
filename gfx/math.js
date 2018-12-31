5f7b38c1d3548bfc6a88f2c724ab4408c66f51a0
tripzilch <tripzilch@gmail.com>
2018-12-31 03:16:35 +0100

    growth physics working!!

--- gfx/math.js
+++ gfx/math.js
@@ -1,37 +1,37 @@
 'use strict';
 
 const PI = Math.PI, TAU = PI * 2;
-// let sin = Math.sin, cos = Math.cos, tan = Math.tan, floor = Math.floor, random = Math.random;
+// let sin = Math.sin, cos = Math.cos, tan = Math.tan, floor = Math.floor;
 // let max = Math.max, min = Math.min, abs = Math.abs, hypot = Math.hypot, sqrt = Math.sqrt;
-let mix = (a, b, p) => a * (1 - p) + b * p;
-let clamp = (x, lo, hi) => Math.min(hi, Math.max(lo, x));
-let smoothstep = (x, e0, e1) => {
+const mix = (a, b, p) => a * (1 - p) + b * p;
+const clamp = (x, lo, hi) => Math.min(hi, Math.max(lo, x));
+const smoothstep = (x, e0, e1) => {
   x = clamp((x - e0) / (e1 - e0), 0, 1);
   return x * x * (3 - 2 * x);
 }
 
 // circular array / modular arithmetic
-let wrap = (a, m) => (a % m + m) % m;
-let iwrap = (a, m) => Math.floor((a % m + m) % m);
-let awrap = (a, i) => a[iwrap(i, a.length)];
-let dmod = (a, b, m) => Math.min(wrap(a - b, m), wrap(b - a, m));
+const wrap = (a, m) => (a % m + m) % m;
+const iwrap = (a, m) => Math.floor((a % m + m) % m);
+const awrap = (a, i) => a[iwrap(i, a.length)];
+const dmod = (a, b, m) => Math.min(wrap(a - b, m), wrap(b - a, m));
 
 // random functions
-let rand = (lo=0, hi=1) => lo + (hi - lo) * Math.random();
-let irand = (lo, hi) => Math.floor(lo + (hi - lo) * Math.random());
-let nrand = (lo=-1, hi=1) => lo + (hi - lo) * .5 * (1 + Math.random() - Math.random());
-let sample = (a) => a[irand(0, a.length)];
-let chance = (p) => rand() < p;
-function shuffle(a) {
+const RNG = Math.random; // todo: PCG random
+const rand = (lo=0, hi=1) => lo + (hi - lo) * RNG();
+const irand = (lo, hi) => lo + Math.floor((hi - lo) * RNG()); // exclusive
+const nrand = (lo=-1, hi=1) => lo + (hi - lo) * .5 * (1 + RNG() - RNG());
+const sample = (a) => a[Math.floor(RNG() * a.length)];
+const chance = (p) => RNG() < p;
+const shuffle = (a) => {
     for (let i = a.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+        let j = Math.floor(RNG() * (i + 1));
         let x = a[i];
         a[i] = a[j];
         a[j] = x;
     }
     return a;
 }
-
 
 class Vec2 {
   constructor(x, y) {
@@ -47,7 +47,7 @@ class Vec2 {
   rot90() { [this.x, this.y] = [-this.y, this.x]; return this; }
   rot180() { this.x *= -1; this.y *= -1; return this; }
   rot270() { [this.x, this.y] = [this.y, -this.x]; return this; }
-  normalize() { let h = 1 / Math.hypot(this.x, this.y); this.x *= h; this.y *= h; return this; }
+  normalize() { let m = 1 / Math.hypot(this.x, this.y); this.x *= m; this.y *= m; return this; }
   op1(f) { this.x = f(this.x); this.y = f(this.y); return this; }
   op2(v, f) { this.x = f(this.x, v.x); this.y = f(this.y, v.y); return this; }
   op3(v, w, f) { this.x = f(this.x, v.x, w.x); this.y = f(this.y, v.y, w.y); return this; }
@@ -64,9 +64,28 @@ class Vec2 {
   set xy(v) { [this.x, this.y] = [v.x, v.y]; }
   set yx(v) { [this.x, this.y] = [v.y, v.x]; }
 
+  // polar getters and setters
+  get r() { return Math.hypot(this.x, this.y); }
+  get phi() { return Math.atan2(this.y, this.x); }
+  set r(r_) { 
+    let m = r_ / Math.hypot(this.x, this.y); 
+    this.x *= m; this.y *= m; 
+  }
+  set phi(phi_) { 
+    let r = Math.hypot(this.x, this.y);
+    this.x = r * Math.cos(phi_);
+    this.y = r * Math.sin(phi_);
+  }
+
+  // iterator
+  *[Symbol.iterator]() {
+    yield this.x;
+    yield this.y;
+  }
+
   // static initializers
-  static random(a=1, rng=Math.random) {
-    return new Vec2(a * rng(), a * rng());
+  static random(a=1) {
+    return new Vec2(a * RNG(), a * RNG());
   }
 
 }
